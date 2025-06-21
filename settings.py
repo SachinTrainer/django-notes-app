01d8d1ef4f126567d8c761ba4ea40585145dfb2e
SachinTrainer <sachin@krtnc.in>
2025-06-21 14:08:33 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', 'notesdb'),
-        'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),  # must be 'db' not 'db_cont'
         'PORT': '3306',
     }
 }
